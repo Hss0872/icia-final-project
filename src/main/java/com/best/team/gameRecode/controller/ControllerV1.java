724c102fc1c0e5443d46f36bb7195f22a6599f45
DESKTOP-3JS1HJR\tjdnf <tjdnf7932@gmail.com>
2022-01-18 20:12:48 +0900

    화이팅

--- src/main/java/com/best/team/gameRecode/controller/ControllerV1.java
+++ src/main/java/com/best/team/gameRecode/controller/ControllerV1.java
@@ -5,24 +5,12 @@ package com.best.team.gameRecode.controller;
 
 import com.best.team.gameRecode.service.SummonerInfoMM;
 
-import com.best.team.image.bean.ChampionDto;
-import com.best.team.image.bean.ItemDto;
-import com.best.team.image.bean.RuneDto;
-import com.best.team.image.bean.SkillDto;
 import com.best.team.image.sevice.ImgMM;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/summoner")
@@ -47,7 +35,7 @@ public class ControllerV1 {
         mav=new ModelAndView();
 
         mav.addObject("summonerName",userName);
-        mav.setViewName("summoner_info_test");
+        mav.setViewName("summoner_info");
         //mav=sm.getSummonerGameRecode(userName,mav);
 
         return mav;
